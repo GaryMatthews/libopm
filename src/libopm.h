89361330f5adcd52bee4574694728660e85bc2d4
strtok <strtok@blitzed.org>
2002-09-04 06:36:07 +0000

    Seperated API and non API definitions between opm.h and libopm.h respectively

--- src/libopm.h
+++ src/libopm.h
@@ -1,51 +1,18 @@
 #include "config.h"
 #include "opm_common.h"
+#include "opm.h"
 
 #ifndef LIBOPM_H
 #define LIBOPM_H
 
-typedef struct  _OPM_CONFIG           OPM_CONFIG_T;
-typedef struct  _OPM                  OPM_T;
 typedef struct  _OPM_SCAN             OPM_SCAN_T;
-typedef struct  _OPM_REMOTE           OPM_REMOTE_T;
 typedef struct  _OPM_CONNECTION       OPM_CONNECTION_T;
-
 typedef struct  _OPM_PROTOCOL_CONFIG  OPM_PROTOCOL_CONFIG_T;
 typedef struct  _OPM_PROTOCOL         OPM_PROTOCOL_T;
-
-typedef         int                   OPM_ERR_T;
-
-typedef void OPM_CALLBACK_T (OPM_REMOTE_T *, int);
-
-struct _OPM_CONFIG {
-   void **vars;
-};
-
-struct _OPM {
-   OPM_CONFIG_T *config;
-   list_t       *scans;
-   list_t       *protocols;
-};
 
 struct _OPM_SCAN { 
    OPM_REMOTE_T        *remote;
    list_t              *connections;
-};
-
-struct _OPM_REMOTE {
-
-   char               *ip; 
-   struct sockaddr_in *addr;
-
-   OPM_CALLBACK_T     *fun_openproxy;
-   OPM_CALLBACK_T     *fun_negfail;
-   OPM_CALLBACK_T     *fun_end;
-   OPM_CALLBACK_T     *fun_error;
-   OPM_CALLBACK_T     *fun_timeout;
-
-   unsigned short int  port;
-   unsigned short int  protocol;                  
-   unsigned short int  bytes_read; 
 };
 
 struct _OPM_CONNECTION {
@@ -71,14 +38,5 @@ struct _OPM_PROTOCOL
    int type;
    int function;  //Replace later with function ptr to protocols
 };
-
-OPM_T *opm_create();
-void opm_free(OPM_T *);
-
-OPM_REMOTE_T *opm_remote_create();
-void opm_remote_free(OPM_REMOTE_T *);
-
-OPM_ERR_T opm_config(OPM_T *, int, void *);
-OPM_ERR_T opm_scan(OPM_T *, OPM_REMOTE_T *);
 
 #endif /* LIBOPM_H */
---
+++ src/opm.h
@@ -0,0 +1,49 @@
+#ifndef OPM_H
+#define OPM_H
+
+#include "opm_common.h"
+
+typedef struct  _OPM_CONFIG           OPM_CONFIG_T;
+typedef struct  _OPM                  OPM_T;
+typedef struct  _OPM_REMOTE           OPM_REMOTE_T;
+
+typedef         int                   OPM_ERR_T;
+
+typedef void OPM_CALLBACK_T (OPM_REMOTE_T *, int);
+
+struct _OPM_CONFIG {
+   void **vars;
+};
+
+struct _OPM {
+   OPM_CONFIG_T *config;
+   list_t       *scans;
+   list_t       *protocols;
+};
+
+struct _OPM_REMOTE {
+
+   char               *ip;
+   struct sockaddr_in *addr;
+
+   OPM_CALLBACK_T     *fun_openproxy;
+   OPM_CALLBACK_T     *fun_negfail;
+   OPM_CALLBACK_T     *fun_end;
+   OPM_CALLBACK_T     *fun_error;
+   OPM_CALLBACK_T     *fun_timeout;
+
+   unsigned short int  port;
+   unsigned short int  protocol;
+   unsigned short int  bytes_read;
+};
+
+OPM_T *opm_create();
+void opm_free(OPM_T *);
+
+OPM_REMOTE_T *opm_remote_create();
+void opm_remote_free(OPM_REMOTE_T *);
+
+OPM_ERR_T opm_config(OPM_T *, int, void *);
+OPM_ERR_T opm_scan(OPM_T *, OPM_REMOTE_T *);
+
+#endif /* OPM_H */
