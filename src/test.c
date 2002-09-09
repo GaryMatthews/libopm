b8147816804bfb5d6bdd46106e151daedf30c969
strtok <strtok@blitzed.org>
2002-09-09 18:07:50 +0000

    Updated test.c to handle error callbacks

--- src/test.c
+++ src/test.c
@@ -11,11 +11,15 @@ void open_proxy(OPM_REMOTE_T *, int);
 void negotiation_failed(OPM_REMOTE_T *, int);
 void timeout(OPM_REMOTE_T *, int);
 void end(OPM_REMOTE_T *, int);
+void handle_error(OPM_REMOTE_T *, int);
 
 int main(int argc, char **argv)
 {
    int fdlimit = 1024;
    int scan_port = 6667;
+   int max_read = 4096;
+   int scantimeout  = 30;
+
    OPM_T *scanner;
    OPM_REMOTE_T *remote;
 
@@ -31,11 +35,14 @@ int main(int argc, char **argv)
    remote->fun_negfail   = &negotiation_failed;
    remote->fun_timeout   = &timeout;
    remote->fun_end       = &end;
+   remote->fun_error     = &handle_error;
  
    opm_config(scanner, OPM_CONFIG_FD_LIMIT, &fdlimit);
    opm_config(scanner, OPM_CONFIG_SCAN_IP, "203.56.139.100");
    opm_config(scanner, OPM_CONFIG_SCAN_PORT, &scan_port);
    opm_config(scanner, OPM_CONFIG_TARGET_STRING, "*** Looking up your hostname...");
+   opm_config(scanner, OPM_CONFIG_TIMEOUT, &scantimeout);
+   opm_config(scanner, OPM_CONFIG_MAX_READ, &max_read);
 
    opm_addtype(scanner, OPM_TYPE_HTTP, 8080);
    opm_addtype(scanner, OPM_TYPE_HTTP, 80);
@@ -75,4 +82,16 @@ void end(OPM_REMOTE_T *remote, int notused)
 {
    printf("Scan on %s has ended\n", remote->ip);
    opm_remote_free(remote);
+}
+
+void handle_error(OPM_REMOTE_T *remote, int err)
+{
+   switch(err)
+   {
+      case OPM_ERR_MAX_READ:
+         printf("Reached MAX READ on %s:%d\n", remote->ip, remote->port);
+         break;
+      default:
+         printf("Unknown error on %s:%d, err = %d\n", remote->ip, remote->port, err);
+   }
 }
