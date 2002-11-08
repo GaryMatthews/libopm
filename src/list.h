f78fdd29c21aaf4c959b58ca85fec721c06cfe9f
strtok <strtok@blitzed.org>
2002-11-08 02:56:03 +0000

    Added proper GPL to hybrid copyright

--- src/list.h
+++ src/list.h
@@ -3,8 +3,27 @@
 
 
 /*  Copyright (C) 2002 by the past and present ircd coders, and others.
-    Adapted from Hybrid7 DLINK macros
-*/
+ *   Adapted from Hybrid7 DLINK macros
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to
+ *
+ *       The Free Software Foundation, Inc.
+ *       59 Temple Place - Suite 330
+ *       Boston, MA  02111-1307, USA.
+ *
+ */
+
 #define LIST_FOREACH(pos, head) for (pos = (head); pos != NULL; pos = pos->next)
 #define LIST_FOREACH_SAFE(pos, n, head) for (pos = (head), n = pos ? pos->next : NULL; pos != NULL; pos = n, n = pos ? pos->next : NULL)
 #define LIST_FOREACH_PREV(pos, head) for (pos = (head); pos != NULL; pos = pos->prev)
